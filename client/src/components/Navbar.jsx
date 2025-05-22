e30eaff498db414b1ef03d90256c6b0263cfe9d4
dangerose777 <dangerose777@gmail.com>
2025-05-23 00:31:23 +0200

    added some items to ui 2

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { MdAccountCircle, MdSearch } from "react-icons/md";
 
 export default function Navbar() {
+  const [inputVisible, setInputVisible] = useState(false);
+  const [animateBorder, setAnimateBorder] = useState(false);
+
+  const toggleSearch = () => {
+    if (inputVisible) {
+      setAnimateBorder(false);
+      setTimeout(() => setInputVisible(false), 300);
+    } else {
+      setInputVisible(true);
+      setTimeout(() => setAnimateBorder(true), 10);
+    }
+  };
+
   return (
     <div className="navbar">
-        <div className="logo">
-            <Link to="/">dangerose777 shop</Link>
+      <div className="logo">
+        <Link to="/">dangerose777 shop</Link>
+      </div>
+
+      {inputVisible && (
+        <div className="searchInputHolder">
+          <input type="text" id="searchInput" />
+          <span className={`bottom-border ${animateBorder ? "animate" : ""}`}></span>
         </div>
-        <div className="search">
-            <button><MdSearch id="buttonSearch"/></button>
-        </div>
-        <div className="account">
-            <Link to="/login"><MdAccountCircle /></Link>
-        </div>
+      )}
+
+      <div className={`search ${inputVisible ? "search--active" : "search--inactive"}`}>
+        <button onClick={toggleSearch}>
+          <MdSearch id="buttonSearch" />
+        </button>
+      </div>
+
+      <div className="account">
+        <Link to="/login"><MdAccountCircle /></Link>
+      </div>
     </div>
   );
 }
