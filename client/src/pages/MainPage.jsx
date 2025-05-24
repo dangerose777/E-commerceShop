6b0c8a590f0ebb09d84b3b3fef1c7b38ebeff216
dangerose777 <dangerose777@gmail.com>
2025-05-24 14:30:44 +0200

    main page ui & functionality, added API

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./MainPage.css";
+import "../App.css";
+
 function MainPage() {
-  return <h1>Main Page</h1>;
+  const API_URL = import.meta.env.VITE_API_URL;
+  //const API_URL_noEnv = "http://localhost:8080";
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/products`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error("Error while getting products:", err))
+  }, [API_URL]);
+
+  return (
+    <>
+    {/* <div className="baner">
+
+    </div> */}
+
+    <div className="products_container">
+      {products.map((product) => (
+        <div className="product_card" key={product.id}>
+          <div className="card" onClick={() => navigate(`/product/${product.id}`)}>
+            <img src={product.image} alt={product.name} />
+            <h3 className="product_name">{product.name}</h3>
+            <p className="product_price">{product.price.toFixed(2)} $</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    </>
+  );
 }
 
 export default MainPage;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
 const corsOptions = {
     origin: ["http://localhost:5173"],
 };
-
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-    res.json({test: ["1", "2", "3"] });
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.get("/api/products", (req, res) => {
+    const dataPath = path.join(__dirname, "api", "products.json");
+    fs.readFile(dataPath, "utf8", (err, data) => {
+        if (err) {
+            console.error("Error while reading file:", err);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 app.listen(8080, () => {
